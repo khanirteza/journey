76c32e2146ddc85ef4061625061b5e607b2546bb
Mohammad Irteza Khan <khanirteza@users.noreply.github.com>
2018-10-29 23:11:45 -0700

    Initial commit

---
+++ journey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  journey
+//
+//  Created by Mohammad Irteza Khan on 10/29/18.
+//  Copyright © 2018 !serious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
